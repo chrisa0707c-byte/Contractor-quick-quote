fa9626aa2d62822d2dbb6bad94231020826fd717
chrisa0707c-byte <chris.a0707.c@gmail.com>
2026-09-18 11:40:17 -0400

    Add files via upload

--- frontend.py
+++ frontend.py
@@ -189,6 +189,3 @@ elif page_selection == "🏗️ AI Estimate Engine":
             
         st.markdown("### 🛠️ Regional Labor Costs")
         for labor in data.labor_list:
-                    for labor in data.labor_list:
-            st.write(f"• {labor.item_name} - ${labor.total_item_cost:.2f}")
-
